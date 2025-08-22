92a7c7c7ea1964dbc8374811ce7710b5841a00e7
Uriel Chanduvi <uriel_chanduvi@usmp.pe>
2025-08-22 15:21:36 -0500

    Proyecto inicial MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VeterinariaMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
